695c36cf655fefa43c139b8a4faea646cad1eb13
KJKR <kavinkj2004@gmail.com>
2025-02-10 10:03:00 +0530

    edit

--- server.js
+++ server.js
@@ -7,10 +7,11 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-let lastCommand = "reset"; 
+let lastCommand = "none";
 
 app.get("/latest-command", (req, res) => {
     res.json({ command: lastCommand });
+    lastCommand = "none";
 });
 
 app.post("/send-command", (req, res) => {
